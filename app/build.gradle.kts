a30ca7809c0d4287303a5bcfb56c8357645d4f11
Oxford-Wei <supasupaweiwei@gmail.com>
2025-11-01 18:02:01 +0000

    reverted api private commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
 
-// Read the api key from the local.properties file
-val localProperties = Properties()
-val localPropertiesFile = rootProject.file("local.properties")
-if (localPropertiesFile.exists()) {
-    localProperties.load(FileInputStream(localPropertiesFile))
-}
+//// Read the api key from the local.properties file
+//val localProperties = Properties()
+//val localPropertiesFile = rootProject.file("local.properties")
+//if (localPropertiesFile.exists()) {
+//    localProperties.load(FileInputStream(localPropertiesFile))
+//}
 
 android {
     namespace = "com.example.zerowaste"
@@ -31,8 +31,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        buildConfigField("String", "GEMINI_API_KEY", localProperties.getProperty("GEMINI_API_KEY") ?: "")
+//
+//        buildConfigField("String", "GEMINI_API_KEY", localProperties.getProperty("GEMINI_API_KEY") ?: "")
     }
 
     buildTypes {
@@ -53,7 +53,7 @@ android {
     }
     buildFeatures {
         compose = true
-        buildConfig = true
+//        buildConfig = true
     }
 }
 
--- app/src/main/java/com/example/zerowaste/ui/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/zerowaste/ui/recipe/RecipeViewModel.kt
@@ -26,7 +26,7 @@ class RecipeViewModel @Inject constructor() : ViewModel() {
     private val _uiState = MutableStateFlow<RecipeUiState>(RecipeUiState.Initial)
     val uiState: StateFlow<RecipeUiState> = _uiState
 
-    private val API_KEY = ""
+    private val API_KEY = "AIzaSyBOodH4-cixzkYcoYhlge2tgFFWcV8yNFg"
 
     fun generateRecipes() {
         viewModelScope.launch {
